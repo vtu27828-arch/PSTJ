4daec32a77dc18a69a7f96515ecdfc44a6afa8c8
vtu27828-arch <vtu27828@veltech.edu.in>
2026-09-07 11:54:24 +0530

    Add Alternating Characters solution in Java
    Implement a solution to count deletions for alternating characters.

---
+++ week 5/session 3/Alternating Characters.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int alternatingCharacters(String s) {
+
+        int deletions = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                deletions++;
+            }
+        }
+
+        return deletions;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            String s = sc.next();
+
+            System.out.println(
+                alternatingCharacters(s)
+            );
+        }
+
+        sc.close();
+    }
+}
+
+Input
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+Output
+3
+4
+0
+0
+4
+
