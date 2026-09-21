c2e06c966e10b01cad54452cd06c4c718c1afaeb
vtu27828-arch <vtu27828@veltech.edu.in>
2026-09-21 12:18:09 +0530

    Create java generics.java

---
+++ week 7/session 5/task 9/java generics.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Printer {
+    public <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+    }
+}
+
+Your Output (stdout)
+1
+2
+3
+Hello
+World
